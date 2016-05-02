f4f6678d41b8a95ef876a1e177d69e2daef47e87
andreskwan <andreskwan@gmail.com>
2016-05-02 14:40:23 -0500

    JSON - [hearthstone - 1, 2, 3 points solved] ToDo - what about return in guard when we don't need it? what about break?

--- hearthstonePG/hearthstone.playground/Contents.swift
+++ hearthstonePG/hearthstone.playground/Contents.swift
@@ -17,7 +17,7 @@ var rawHearthstoneJSON = NSData(contentsOfFile: pathForHearthstoneJSON!)
 /* Error object */
 var parsingHearthstoneError: NSError? = nil
 
-/* Parse the data into usable form */
+/* Parse the data Floato usable form */
 var parsedHearthstoneJSON = try! NSJSONSerialization.JSONObjectWithData(rawHearthstoneJSON!, options: .AllowFragments) as! NSDictionary
 
 func parseJSONAsDictionary(dictionary: NSDictionary) {
@@ -28,9 +28,18 @@ func parseJSONAsDictionary(dictionary: NSDictionary) {
     }
     
     print(arrayOfGameElements.count)
-    var minionsAtFiveDollars = 0
+    // 1
+    var minionsAtFiveDollars = Float(0)
+    // 3
     var minionsWithBattleCry = 0
-    var minionCostArray = [Int]()
+    
+    var totalMiniosCost = Float(0)
+    var totalNumberOfMinions = Float(0)
+
+    var totalCostWithoutCeroCostMinions = Float(0)
+    var totalAttack = Float(0)
+    var totalHealt = Float(0)
+    
     for (index, gameElementDictionary) in arrayOfGameElements.enumerate() {
         guard let type = gameElementDictionary["type"] as? String else {
             print("can't find type")
@@ -38,26 +47,53 @@ func parseJSONAsDictionary(dictionary: NSDictionary) {
         }
         
         if type == "Minion" {
-            guard let minionCost = gameElementDictionary["cost"] as? Int else {
+            // 1 - How many minions have a cost of 5?
+            guard let minionCost = gameElementDictionary["cost"] as? Float else {
                 print("can't find minion cost")
                 return
             }
-            if minionCost >= 5 {
+            if minionCost == 5 {
                 minionsAtFiveDollars = minionsAtFiveDollars + 1
                 print("index: \(index), cost: \(minionCost)")
             }
-            
+            // 3 - How many minons have a "Battlecry" effect
+            if let text = gameElementDictionary["text"] as? String {
+                if text.rangeOfString("Battlecry") != nil {
+                    minionsWithBattleCry = minionsWithBattleCry + 1
+                    print("index: \(index)")
+                    print("text: \(text)")
+                    
+                }
+            }
+            // 4 - average cost of common minions
+            guard let attack = gameElementDictionary["attack"] as? Float else {
+                print("can't find attack for minion at index: \(index)")
+                break
+            }
+            guard let health = gameElementDictionary["health"] as? Float else {
+                print("can't find health for minion at index: \(index)")
+                break
+            }
+            // problem 
+            guard let rarity = gameElementDictionary["Common"] as? String else {
+                print("can't find rarity common for minion at index: \(index)")
+                break
+            }
+            if rarity == "common" {
+                totalMiniosCost = totalMiniosCost + minionCost
+                totalNumberOfMinions = totalNumberOfMinions + 1
+                print("index: \(index)")
+            }
             print(index)
             print(minionCost)
-            guard let text = gameElementDictionary["text"] as? String else {
-                print("can't find values for \"text\" key")
-                return
-            }
-            if text.rangeOfString("Battlecry") != nil {
-                minionsWithBattleCry = minionsWithBattleCry + 1
-                print(text)
+            // 5 - what is the average stats-to-cost-ratio = (attack + health)/cost
+            if minionCost > 0.0 {
+                totalCostWithoutCeroCostMinions = totalCostWithoutCeroCostMinions + minionCost
+                totalAttack = totalAttack + attack
+                totalHealt = totalHealt + health
             }
         }
+        // 2 - How many weapons have a durability of 2?
         if type == "Weapon" {
             guard let durability = gameElementDictionary["durability"] as? Int else {
                 print("can't find durabily of the element")
@@ -69,7 +105,9 @@ func parseJSONAsDictionary(dictionary: NSDictionary) {
         }
     }
     print(minionsWithBattleCry)
-    print(minionsAtFiveDollars)
+//    print(minionsAtFiveDollars)
+//    print("average cost = \(totalMiniosCost)/\(totalNumberOfMinions) = \(totalMiniosCost/totalNumberOfMinions) ")
+//    print("stats-to-cost-ratio = (\(totalAttack)+\(totalHealt))/\(totalNumberOfMinions) = \( (totalAttack + totalHealt)/totalNumberOfMinions) ")
 }
 
 struct Constants {
