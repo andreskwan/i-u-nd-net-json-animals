7ca8523bba947751c6e63f2f8a1b6c7b069c07c5
andreskwan <andreskwan@gmail.com>
2016-05-03 17:16:34 -0500

    JSON - [achievements - 1, 2, 3 point solved]

--- achievementsPG/achievements.playground/Contents.swift
+++ achievementsPG/achievements.playground/Contents.swift
@@ -22,6 +22,42 @@ var parsedAchievementsJSON = try! NSJSONSerialization.JSONObjectWithData(rawAchi
 
 func parseJSONAsDictionary(dictionary: NSDictionary) {
     /* Start playing with JSON here... */
+    // 1 achievements have a point value greater than 10
+    guard let achievements = parsedAchievementsJSON["achievements"] as? [[String:AnyObject]] else {
+        print("can't find achievements in \(parsedAchievementsJSON)")
+        return
+    }
+    
+    // 1
+    var achievementsWithPointValueGreaterThan10 = 0
+    // 2
+    var sumOfPointValue = 0
+    var achievementsWithPoints = 0
+    
+    for achievement in achievements {
+        guard let points = achievement["points"] as? Int else {
+            return
+        }
+        // 1
+        if points > 10 {
+            achievementsWithPointValueGreaterThan10 += 1
+        }
+        // 2.0
+        sumOfPointValue += points
+        achievementsWithPoints += 1
+        
+        // 3 what mission for achievement "Cool Running"
+        if let title = achievement["title"] as? String where title == "Cool Running" {
+            guard let description = achievement["description"] as? String else {
+                return
+            }
+            print(description)
+        }
+    }
+    // 1 
+    print(achievementsWithPointValueGreaterThan10)
+    // 2.1 average
+    print(Double(sumOfPointValue)/Double(achievementsWithPoints))
 }
 
 parseJSONAsDictionary(parsedAchievementsJSON)
